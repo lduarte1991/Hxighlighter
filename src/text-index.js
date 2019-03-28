1932053920c9cf22bd63cec34d9d02ce463b2860
lduarte1991 <lduarte1991@gmail.com>
2019-03-28 14:03:15 -0400

    set up webpack css and fonts
    Added in bootstrap and fontawesome dependencies.

---
+++ src/text-index.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+require('bootstrap/dist/js/bootstrap.min.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,46 @@
 const path = require('path');
 var webpack = require('webpack');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry: {
         text: ['./src/text-index.js']
-    }
-    plugins: [],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'dist/hxighlighter_[name].css',
+            chunkFilename: "[id].css"
+        }),
+    ],
     output: {
         path: __dirname,
-        filename: 'dist/hxighlighter.js'
+        filename: 'dist/hxighlighter_[name].js'
     },
     resolve: {
         extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(eot|gif|svg|png|jpg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+                use: require.resolve('url-loader')
+            },
+        ]
+    },
+    performance: {
+        'hints': false
     }
 }
