19dd3bd2889732748d659cac821185be8828df8f
lduarte1991 <lduarte1991@gmail.com>
2026-04-22 19:37:51 -0400

    feat: add lint config

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+        ...globals.commonjs,
+        jQuery: "readonly",
+        $: "readonly",
+        Hxighlighter: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { args: "none", caughtErrors: "none" }],
+      "no-undef": "error",
+      "no-console": "warn",
+      "eqeqeq": ["warn", "smart"],
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-new-func": "error",
+      "no-var": "off",
+      "prefer-const": "warn",
+    },
+  },
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
+  {
+    files: ["webpack.config.js", "server.js", "eslint.config.mjs"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ["dist/", "coverage/", "node_modules/", "src/js/vendors/"],
+  },
+];
