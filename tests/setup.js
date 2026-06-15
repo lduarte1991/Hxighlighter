be08eb3bd62fe5184c8f56452f4214ac33490278
lduarte1991 <lduarte1991@gmail.com>
2026-06-15 18:10:57 -0400

    fix: update test with new node and puppeteer versions

--- tests/setup.js
+++ tests/setup.js
@@ -3,7 +3,11 @@ const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
 
 global.window = dom.window;
 global.document = dom.window.document;
-global.navigator = dom.window.navigator;
+Object.defineProperty(global, 'navigator', {
+  value: dom.window.navigator,
+  writable: true,
+  configurable: true,
+});
 
 var jQuery = require('jquery');
 global.jQuery = jQuery;
