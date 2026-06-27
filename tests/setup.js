41fe2da6187596acbf4bcd669d3d5a500648f364
lduarte1991 <lduarte1991@gmail.com>
2026-06-27 00:33:12 -0400

    test: core/h-range.js test enhancements (#77)
    * docs: adding CLAUDE.md
    
    * docs: added comments to h-range.js explaining the core funcs
    
    * feat: added initial tests for h-range
    
    * docs: added item on comments

--- tests/setup.js
+++ tests/setup.js
@@ -3,11 +3,8 @@ const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
 
 global.window = dom.window;
 global.document = dom.window.document;
-Object.defineProperty(global, 'navigator', {
-  value: dom.window.navigator,
-  writable: true,
-  configurable: true,
-});
+global.navigator = dom.window.navigator;
+global.Node = dom.window.Node;
 
 var jQuery = require('jquery');
 global.jQuery = jQuery;
